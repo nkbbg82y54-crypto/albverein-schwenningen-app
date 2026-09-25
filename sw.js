f85ad47473190ab246a5720dd29eb9f23c84c416
nkbbg82y54-crypto <nkbbg82y54@privaterelay.appleid.com>
2026-09-25 10:20:08 -0700

    PWA-Cache für Rollenupdate erneuern

--- sw.js
+++ sw.js
@@ -6,3 +6,11 @@ self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
 });
+const CACHE='albverein-schwenningen-v2';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/florian-lessner.jpeg','./assets/matthias-kienitz.jpeg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
